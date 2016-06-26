4d57207231f654a4a797f61b6c0c2abc005da4c8
nkl07ba <nikhillance16@gmail.com>
2016-06-26 23:36:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nikhil/sunshine/Detail_activityFragment.java
@@ -0,0 +1,22 @@
+package com.example.nikhil.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Detail_activityFragment extends Fragment {
+
+    public Detail_activityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
