43eec9d4d95eeace1c3227e968228ceb8d65f7a8
nikhil-31 <nkl07ba@gmail.com>
2016-07-24 16:28:22 +0530

    Added additional files cause the course wanted me to copy pasta

---
+++ app/src/androidTest/java/FullTestSuite.java
@@ -0,0 +1,19 @@
+/**
+ * Created by nikhil on 22-07-2016.
+ */
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+        import junit.framework.Test;
+        import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
